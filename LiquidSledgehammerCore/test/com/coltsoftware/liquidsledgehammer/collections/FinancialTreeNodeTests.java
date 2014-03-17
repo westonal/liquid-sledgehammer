1210732a94a8238908b8fa137412ad02585ed05c
Alan Evans <thealanevans@googlemail.com>
2014-03-17 19:43:32 +0000

    Is a true tree now. Does not allow us to define a named path yet.

--- LiquidSledgehammerCore/test/com/coltsoftware/liquidsledgehammer/collections/FinancialTreeNodeTests.java
+++ LiquidSledgehammerCore/test/com/coltsoftware/liquidsledgehammer/collections/FinancialTreeNodeTests.java
@@ -7,11 +7,10 @@ import org.junit.Test;
 
 import com.coltsoftware.liquidsledgehammer.BaseTest;
 import com.coltsoftware.liquidsledgehammer.model.FinancialTransaction;
-import com.coltsoftware.liquidsledgehammer.model.FinancialTransaction.Builder;
 
 public final class FinancialTreeNodeTests extends BaseTest {
 
-	private FinancialTreeNode ftn;
+	private FinancialTreeNode root;
 
 	private FinancialTransaction createTransaction(long value) {
 		return new FinancialTransaction.Builder().date(2014, 5, 1).value(value)
@@ -20,28 +19,49 @@ public final class FinancialTreeNodeTests extends BaseTest {
 
 	@Before
 	public void setup() {
-		ftn = new FinancialTreeNode();
+		root = new FinancialTreeNode();
 	}
 
 	@Test
 	public void initial_size() {
-		assertEquals(0, count(ftn.getTransactions()));
-		assertEquals(0, ftn.getTotalValue().getValue());
+		assertEquals(0, count(root.getTransactions()));
+		assertEquals(0, root.getTotalValue().getValue());
 	}
 
 	@Test
 	public void can_add_transaction() {
-		ftn.add(createTransaction(100));
-		assertEquals(1, count(ftn.getTransactions()));
-		assertEquals(100, ftn.getTotalValue().getValue());
+		root.add(createTransaction(100));
+		assertEquals(1, count(root.getTransactions()));
+		assertEquals(100, root.getTotalValue().getValue());
 	}
 
 	@Test
 	public void can_add_2_transactions() {
+		root.add(createTransaction(100));
+		root.add(createTransaction(345));
+		assertEquals(2, count(root.getTransactions()));
+		assertEquals(445, root.getTotalValue().getValue());
+	}
+	
+	@Test
+	public void can_add_another_tree() {
+		FinancialTreeNode ftn = new FinancialTreeNode();
 		ftn.add(createTransaction(100));
-		ftn.add(createTransaction(345));
-		assertEquals(2, count(ftn.getTransactions()));
-		assertEquals(445, ftn.getTotalValue().getValue());
+		root.add(ftn);
+		assertEquals(0, count(root.getTransactions()));
+		assertEquals(100, root.getTotalValue().getValue());
+	}
+	
+	@Test
+	public void can_add_two_other_trees() {
+		FinancialTreeNode ftn1 = new FinancialTreeNode();
+		FinancialTreeNode ftn2 = new FinancialTreeNode();
+		ftn1.add(createTransaction(100));
+		ftn2.add(createTransaction(200));
+		root.add(ftn1);
+		root.add(ftn2);
+		assertEquals(0, count(root.getTransactions()));
+		assertEquals(300, root.getTotalValue().getValue());
 	}
 
 }
