fd19e7cfaaeba4033409076a1d52f3c2f92420b7
Alan Evans <thealanevans@googlemail.com>
2014-03-17 19:43:32 +0000

    Is a true tree now. Does not allow us to define a named path yet.

--- LiquidSledgehammerCore/test/com/coltsoftware/liquidsledgehammer/collections/FinancialTreeNode.java
+++ LiquidSledgehammerCore/test/com/coltsoftware/liquidsledgehammer/collections/FinancialTreeNode.java
@@ -1,5 +1,6 @@
 package com.coltsoftware.liquidsledgehammer.collections;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 
 import com.coltsoftware.liquidsledgehammer.collections.FinancialTransactionList;
@@ -9,9 +10,14 @@ import com.coltsoftware.liquidsledgehammer.model.Money;
 public final class FinancialTreeNode {
 
 	private final FinancialTransactionList transactions = new FinancialTransactionList();
+	private final ArrayList<FinancialTreeNode> children = new ArrayList<FinancialTreeNode>();
 
 	public void add(FinancialTransaction transaction) {
 		transactions.add(transaction);
+	}
+
+	public void add(FinancialTreeNode child) {
+		children.add(child);
 	}
 
 	public Iterator<FinancialTransaction> getTransactions() {
@@ -19,6 +25,10 @@ public final class FinancialTreeNode {
 	}
 
 	public Money getTotalValue() {
-		return transactions.getTotalValue();
+		Money totalValue = transactions.getTotalValue();
+		for (FinancialTreeNode child : children)
+			totalValue = totalValue.add(child.getTotalValue());
+		return totalValue;
 	}
+
 }
