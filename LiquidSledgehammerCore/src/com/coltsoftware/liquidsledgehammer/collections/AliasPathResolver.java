a0e963eb49a49f8711123b514759d947802dda82
Alan Evans <alan.evans@burrows.yr.com>
2014-03-26 13:53:39 +0000

    Refactored AliasPathResolver.

--- LiquidSledgehammerCore/src/com/coltsoftware/liquidsledgehammer/collections/AliasPathResolver.java
+++ LiquidSledgehammerCore/src/com/coltsoftware/liquidsledgehammer/collections/AliasPathResolver.java
@@ -7,25 +7,24 @@ public final class AliasPathResolver {
 	private final HashMap<String, String> mapping = new HashMap<String, String>();
 
 	public String resolve(String alias) {
-		alias = sanitiseAlias(alias);
-		String path = mapping.get(alias);
-		if (path == null) {
-			if (alias.equals(""))
-				return "Error.Uncategorised";
-			return "Error.UnknownAlias." + alias;
-		}
-		return path;
+		String sanatizedAlias = sanitiseAlias(alias);
+		String path = mapping.get(sanatizedAlias);
+		return path == null ? pathToMissingAlias(sanatizedAlias) : path;
 	}
 
 	public void put(String alias, String path) {
-		alias = sanitiseAlias(alias);
-		mapping.put(alias, path);
+		String sanatizedAlias = sanitiseAlias(alias);
+		mapping.put(sanatizedAlias, path);
+	}
+
+	private static String pathToMissingAlias(String sanatizedAlias) {
+		if (sanatizedAlias.equals(""))
+			return "Error.Uncategorised";
+		return "Error.UnknownAlias." + sanatizedAlias;
 	}
 
 	private static String sanitiseAlias(String alias) {
-		if (alias == null)
-			alias = "";
-		return alias;
+		return alias == null ? "" : alias;
 	}
 
 }
