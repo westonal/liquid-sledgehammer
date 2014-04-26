2f4937ac6841c58b1391d593a55d85327c1057d4
Alan Evans <thealanevans@googlemail.com>
2014-04-26 19:56:42 +0100

    Walks tree and discovers json files.

--- LiquidSledgehammerCommandLine/src/com/coltsoftware/liquidsledgehammer/Main.java
+++ LiquidSledgehammerCommandLine/src/com/coltsoftware/liquidsledgehammer/Main.java
@@ -1,14 +1,11 @@
 package com.coltsoftware.liquidsledgehammer;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
+import java.util.ArrayList;
 
 import com.coltsoftware.liquidsledgehammer.collections.AliasPathResolver;
 import com.coltsoftware.liquidsledgehammer.collections.FinancialTreeNode;
-import com.coltsoftware.liquidsledgehammer.json.JsonStreamTransactionSource;
-import com.coltsoftware.liquidsledgehammer.model.FinancialTransaction;
 import com.coltsoftware.liquidsledgehammer.model.SubTransaction;
 import com.coltsoftware.liquidsledgehammer.processing.Processor;
 import com.coltsoftware.liquidsledgehammer.sources.FinancialTransactionSource;
@@ -16,39 +13,26 @@ import com.coltsoftware.liquidsledgehammer.sources.FinancialTransactionSource;
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-		File f = new File(args[0]);
-		InputStream stream = new FileInputStream(f);
-		try {
-			FinancialTransactionSource source = JsonStreamTransactionSource
-					.fromStream(stream);
-			for (FinancialTransaction t : source)
-				output(String.format("%s, %s", t.getDescription(), t.getValue()
-						.toString()));
-
-			Processor processor = new Processor(new AliasPathResolver());
-			FinancialTreeNode root = new FinancialTreeNode();
+		File f = new File("C:\\Temp\\Transactions");
+		ArrayList<FinancialTransactionSource> sources = PathSourceWalker.loadAllSourcesBelowPath(f.toPath());
+		Processor processor = new Processor(new AliasPathResolver());
+		FinancialTreeNode root = new FinancialTreeNode();
+		for (FinancialTransactionSource source : sources)
 			processor.populateTree(source, root);
 
-			output("");
-			outputTree(root);
-		} finally {
-			stream.close();
-		}
+		Output.output("");
+		outputTree(root);
 	}
 
+
 	private static void outputTree(FinancialTreeNode root) {
-		output(String.format("Treenode %s = %s", root.getName(),
+		Output.output(String.format("Treenode %s = %s", root.getName(),
 				root.getTotalValue()));
 		for (SubTransaction transaction : root.getSubTransactions())
-			output(String.format("  %s = %s", transaction.getTransaction()
+			Output.output(String.format("  %s = %s", transaction.getTransaction()
 					.getDescription(), transaction.getValue()));
 
 		for (FinancialTreeNode node : root)
 			outputTree(node);
 	}
-
-	private static void output(String message) {
-		System.out.println(message);
-	}
-
 }
---
+++ LiquidSledgehammerCommandLine/src/com/coltsoftware/liquidsledgehammer/Output.java
@@ -0,0 +1,12 @@
+package com.coltsoftware.liquidsledgehammer;
+
+public final class Output {
+
+	private Output() {
+	}
+
+	public static void output(String message) {
+		System.out.println(message);
+	}
+
+}
