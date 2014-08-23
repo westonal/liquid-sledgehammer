81b1c530efd0692aab6efd82bf19aebc9fdaf395
Alan Evans <thealanevans@googlemail.com>
2014-08-23 11:22:15 +0100

    LogCats the clicked rectangle.

--- LiquidSledgehammerAndroidExample/src/com/coltsoftware/liquidsledgehammer/androidexample/RectDisplay.java
+++ LiquidSledgehammerAndroidExample/src/com/coltsoftware/liquidsledgehammer/androidexample/RectDisplay.java
@@ -2,20 +2,19 @@ package com.coltsoftware.liquidsledgehammer.androidexample;
 
 import java.util.List;
 
-import com.coltsoftware.rectangleareagraph.Rectangle;
-import com.coltsoftware.rectangleareagraph.RectangleSplit;
-import com.coltsoftware.rectangleareagraph.RectangleSplit.SplitResult;
-
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Paint.Style;
 import android.graphics.Rect;
-import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+
+import com.coltsoftware.rectangleareagraph.Rectangle;
+import com.coltsoftware.rectangleareagraph.RectangleSplit;
+import com.coltsoftware.rectangleareagraph.RectangleSplit.SplitResult;
 
 public class RectDisplay extends View {
 
@@ -74,14 +73,28 @@ public class RectDisplay extends View {
 	@Override
 	public boolean onTouchEvent(MotionEvent event) {
 		if (event.getActionMasked() == MotionEvent.ACTION_DOWN) {
-			Log.d(TAG, "DOWN");
 			return true;
 		}
 		if (event.getActionMasked() == MotionEvent.ACTION_UP) {
-			Log.d(TAG, "UP");
+			int x = (int) event.getX();
+			int y = (int) event.getY();
+			SplitResult<String> findItemAt = findItemAt(x, y);
+			if (findItemAt != null)
+				Log.d(TAG,
+						String.format("Clicked %s (%s)",
+								findItemAt.getRectangle(), findItemAt.getTag()));
 			return true;
 		}
 		return super.onTouchEvent(event);
+	}
+
+	private SplitResult<String> findItemAt(int x, int y) {
+		if (split == null)
+			return null;
+		for (SplitResult<String> rect : split)
+			if (rect.getRectangle().inside(x, y))
+				return rect;
+		return null;
 	}
 
 }
