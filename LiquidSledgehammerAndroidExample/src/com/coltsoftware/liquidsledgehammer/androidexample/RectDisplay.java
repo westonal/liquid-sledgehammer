b67c844d260d9c9b97e413c0a3a4399b76b0113c
Alan Evans <thealanevans@googlemail.com>
2014-08-23 10:42:45 +0100

    Android example working with fake data.

---
+++ LiquidSledgehammerAndroidExample/src/com/coltsoftware/liquidsledgehammer/androidexample/RectDisplay.java
@@ -0,0 +1,71 @@
+package com.coltsoftware.liquidsledgehammer.androidexample;
+
+import java.util.List;
+
+import com.coltsoftware.rectangleareagraph.Rectangle;
+import com.coltsoftware.rectangleareagraph.RectangleSplit;
+import com.coltsoftware.rectangleareagraph.RectangleSplit.SplitResult;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Paint.Style;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class RectDisplay extends View {
+
+	private RectangleSplit<String> rectangleSplit;
+	private List<SplitResult<String>> split;
+	private Paint paint = new Paint();
+
+	public RectDisplay(Context context) {
+		super(context);
+		init();
+	}
+
+	public RectDisplay(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		init();
+	}
+
+	public RectDisplay(Context context, AttributeSet attrs, int defStyleAttr) {
+		super(context, attrs, defStyleAttr);
+		init();
+	}
+
+	private void init() {
+		rectangleSplit = new RectangleSplit<String>();
+		rectangleSplit.addValue(10, "A");
+		rectangleSplit.addValue(20, "B");
+		rectangleSplit.addValue(40, "C");
+		rectangleSplit.addValue(70, "D");
+	}
+
+	@Override
+	protected void onDraw(Canvas canvas) {
+		canvas.drawColor(0xff00ff00);
+		if (split == null)
+			split = rectangleSplit.split(new Rectangle(0, 0, getWidth() - 1,
+					getHeight() - 1));
+		paint.setColor(0xff0000ff);
+		paint.setStyle(Style.STROKE);
+		for (SplitResult<String> rect : split)
+			canvas.drawRect(toGraphicsRect(rect.getRectangle()), paint);
+	}
+
+	private Rect toGraphicsRect(Rectangle rectangle) {
+		return new Rect(rectangle.getLeft(), rectangle.getTop(),
+				rectangle.getWidth() + rectangle.getLeft(),
+				rectangle.getHeight() + rectangle.getTop());
+	}
+
+	@Override
+	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+		split = null;
+		super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+	}
+
+}
