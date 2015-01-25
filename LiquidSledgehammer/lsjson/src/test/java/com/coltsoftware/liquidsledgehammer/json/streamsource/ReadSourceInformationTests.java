1568bda62cce0edfefd13edb999cdecf0c7272be
Alan Evans <thealanevans@googlemail.com>
2015-01-25 18:07:55 +0000

    Builds - tests fail - no json data.

--- LiquidSledgehammer/lsjson/src/test/java/com/coltsoftware/liquidsledgehammer/json/streamsource/ReadSourceInformationTests.java
+++ LiquidSledgehammer/lsjson/src/test/java/com/coltsoftware/liquidsledgehammer/json/streamsource/ReadSourceInformationTests.java
@@ -1,53 +1,52 @@
 package com.coltsoftware.liquidsledgehammer.json.streamsource;
 
-import static org.junit.Assert.assertEquals;
-
-import java.util.Iterator;
-
-import org.joda.time.LocalDate;
-import org.junit.Before;
-import org.junit.Test;
-
 import com.coltsoftware.liquidsledgehammer.json.JsonStreamTransactionSource;
 import com.coltsoftware.liquidsledgehammer.model.FinancialTransaction;
 import com.coltsoftware.liquidsledgehammer.model.FinancialTransactionSourceInformation;
-import com.coltsoftware.liquidsledgehammer.model.NullFinancialTransactionSourceInformation;
 import com.coltsoftware.liquidsledgehammer.sources.FinancialTransactionSource;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Iterator;
+
+import static com.coltsoftware.liquidsledgehammer.IterableCounter.count;
+import static org.junit.Assert.assertEquals;
 
 public final class ReadSourceInformationTests extends StreamSourceTestBase {
 
-	@Override
-	protected String getAssetName() {
-		return "statement.json";
-	}
+    @Override
+    protected String getAssetName() {
+        return "statement.json";
+    }
 
-	private FinancialTransactionSource source;
+    private FinancialTransactionSource source;
 
-	@Before
-	@Override
-	public void setup() {
-		super.setup();
-		source = JsonStreamTransactionSource.fromStream(stream,
-				new FinancialTransactionSourceInformation() {
+    @Before
+    @Override
+    public void setup() {
+        super.setup();
+        source = JsonStreamTransactionSource.fromStream(stream,
+                new FinancialTransactionSourceInformation() {
 
-					@Override
-					public String getName() {
-						return "A bank";
-					}
-				});
-	}
+                    @Override
+                    public String getName() {
+                        return "A bank";
+                    }
+                });
+    }
 
-	@Test
-	public void loaded_correct_number() {
-		assertEquals(2, count(source));
-	}
+    @Test
+    public void loaded_correct_number() {
+        assertEquals(2, count(source));
+    }
 
-	@Test
-	public void can_read_source_name() {
-		Iterator<FinancialTransaction> iterator = source.iterator();
-		FinancialTransaction transaction = iterator.next();
-		assertEquals("A bank", transaction.getSource().getName());
-		transaction = iterator.next();
-		assertEquals("A bank", transaction.getSource().getName());
-	}
+    @Test
+    public void can_read_source_name() {
+        Iterator<FinancialTransaction> iterator = source.iterator();
+        FinancialTransaction transaction = iterator.next();
+        assertEquals("A bank", transaction.getSource().getName());
+        transaction = iterator.next();
+        assertEquals("A bank", transaction.getSource().getName());
+    }
 }
--- LiquidSledgehammer/lsjson/src/test/java/com/coltsoftware/liquidsledgehammer/json/streamsource/StreamSourceTestBase.java
+++ LiquidSledgehammer/lsjson/src/test/java/com/coltsoftware/liquidsledgehammer/json/streamsource/StreamSourceTestBase.java
@@ -1,33 +1,31 @@
 package com.coltsoftware.liquidsledgehammer.json.streamsource;
 
-import java.io.IOException;
-import java.io.InputStream;
-
 import org.junit.After;
 import org.junit.Before;
 
-import com.coltsoftware.liquidsledgehammer.MoneyTestBase;
+import java.io.IOException;
+import java.io.InputStream;
 
 public abstract class StreamSourceTestBase {
 
-	public static InputStream loadAsset(String assetName) {
-		InputStream stream = Thread.currentThread().getContextClassLoader()
-				.getResourceAsStream(assetName);
-		return stream;
-	}
+    public static InputStream loadAsset(String assetName) {
+        InputStream stream = Thread.currentThread().getContextClassLoader()
+                .getResourceAsStream(assetName);
+        return stream;
+    }
 
-	protected InputStream stream;
+    protected InputStream stream;
 
-	@Before
-	public void setup() {
-		stream = loadAsset(getAssetName());
-	}
+    @Before
+    public void setup() {
+        stream = loadAsset(getAssetName());
+    }
 
-	@After
-	public void teardown() throws IOException {
-		stream.close();
-	}
+    @After
+    public void teardown() throws IOException {
+        stream.close();
+    }
 
-	protected abstract String getAssetName();
+    protected abstract String getAssetName();
 
 }
