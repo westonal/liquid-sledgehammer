54db01b2b43591927cd32611292e987d9ef1ddf1
Alan Evans <thealanevans@googlemail.com>
2015-01-25 18:04:52 +0000

    Could not inherit from test in other project. So made helper classes.

--- LiquidSledgehammer/core/src/test/java/com/coltsoftware/liquidsledgehammer/BaseTest.java
+++ LiquidSledgehammer/core/src/test/java/com/coltsoftware/liquidsledgehammer/BaseTest.java
@@ -1,44 +1,39 @@
 package com.coltsoftware.liquidsledgehammer;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
+import org.junit.Before;
 
 import java.util.Iterator;
 import java.util.Locale;
 
-import org.junit.Before;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
 
 public abstract class BaseTest {
 
-	@Before
-	public void setDefaultLocale() {
-		Locale.setDefault(Locale.UK);
-	}
+    @Before
+    public void setDefaultLocale() {
+        Locale.setDefault(Locale.UK);
+    }
 
-	protected static void assertNotEqual(Object a, Object b) {
-		assertFalse(equals(a, b));
-	}
+    protected static void assertNotEqual(Object a, Object b) {
+        assertFalse(equals(a, b));
+    }
 
-	protected static boolean equals(Object a, Object b) {
-		if (a == b)
-			return true;
-		if (a == null)
-			return false;
-		return a.equals(b);
-	}
+    protected static boolean equals(Object a, Object b) {
+        if (a == b)
+            return true;
+        if (a == null)
+            return false;
+        return a.equals(b);
+    }
 
-	protected static int count(Iterable<?> iterable) {
-		return count(iterable.iterator());
-	}
+    protected static int count(Iterable<?> iterable) {
+        return IterableCounter.count(iterable.iterator());
+    }
 
-	protected static int count(Iterator<?> iterator) {
-		assertNotNull(iterator);
-		int count = 0;
-		while (iterator.hasNext()) {
-			iterator.next();
-			count++;
-		}
-		return count;
-	}
+    protected static int count(Iterator<?> iterator) {
+        assertNotNull(iterator);
+        return IterableCounter.count(iterator);
+    }
 
 }
--- LiquidSledgehammer/lsjson/src/test/java/com/coltsoftware/liquidsledgehammer/json/streamsource/ReadingTests.java
+++ LiquidSledgehammer/lsjson/src/test/java/com/coltsoftware/liquidsledgehammer/json/streamsource/ReadingTests.java
@@ -1,73 +1,75 @@
 package com.coltsoftware.liquidsledgehammer.json.streamsource;
 
-import static org.junit.Assert.assertEquals;
-
-import java.util.Iterator;
-
-import org.joda.time.LocalDate;
-import org.junit.Before;
-import org.junit.Test;
-
 import com.coltsoftware.liquidsledgehammer.json.JsonStreamTransactionSource;
 import com.coltsoftware.liquidsledgehammer.model.FinancialTransaction;
 import com.coltsoftware.liquidsledgehammer.model.NullFinancialTransactionSourceInformation;
 import com.coltsoftware.liquidsledgehammer.sources.FinancialTransactionSource;
 
+import org.joda.time.LocalDate;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Iterator;
+
+import static com.coltsoftware.liquidsledgehammer.IterableCounter.count;
+import static com.coltsoftware.liquidsledgehammer.model.MoneyHelper.local;
+import static org.junit.Assert.assertEquals;
+
 public final class ReadingTests extends StreamSourceTestBase {
 
-	@Override
-	protected String getAssetName() {
-		return "statement.json";
-	}
+    @Override
+    protected String getAssetName() {
+        return "statement.json";
+    }
 
-	private FinancialTransactionSource source;
+    private FinancialTransactionSource source;
 
-	@Before
-	@Override
-	public void setup() {
-		super.setup();
-		source = JsonStreamTransactionSource.fromStream(stream,
-				NullFinancialTransactionSourceInformation.INSTANCE);
-	}
+    @Before
+    @Override
+    public void setup() {
+        super.setup();
+        source = JsonStreamTransactionSource.fromStream(stream,
+                NullFinancialTransactionSourceInformation.INSTANCE);
+    }
 
-	@Test
-	public void loaded_correct_number() {
-		assertEquals(2, count(source));
-	}
+    @Test
+    public void loaded_correct_number() {
+        assertEquals(2, count(source));
+    }
 
-	@Test
-	public void can_read_values() {
-		Iterator<FinancialTransaction> iterator = source.iterator();
-		FinancialTransaction transaction = iterator.next();
-		assertEquals(local(987), transaction.getValue());
-		transaction = iterator.next();
-		assertEquals(local(912), transaction.getValue());
-	}
+    @Test
+    public void can_read_values() {
+        Iterator<FinancialTransaction> iterator = source.iterator();
+        FinancialTransaction transaction = iterator.next();
+        assertEquals(local(987), transaction.getValue());
+        transaction = iterator.next();
+        assertEquals(local(912), transaction.getValue());
+    }
 
-	@Test
-	public void can_read_descriptions() {
-		Iterator<FinancialTransaction> iterator = source.iterator();
-		FinancialTransaction transaction = iterator.next();
-		assertEquals("Desc for item 1", transaction.getDescription());
-		transaction = iterator.next();
-		assertEquals("Desc for item 2", transaction.getDescription());
-	}
+    @Test
+    public void can_read_descriptions() {
+        Iterator<FinancialTransaction> iterator = source.iterator();
+        FinancialTransaction transaction = iterator.next();
+        assertEquals("Desc for item 1", transaction.getDescription());
+        transaction = iterator.next();
+        assertEquals("Desc for item 2", transaction.getDescription());
+    }
 
-	@Test
-	public void can_read_groups() {
-		Iterator<FinancialTransaction> iterator = source.iterator();
-		FinancialTransaction transaction = iterator.next();
-		assertEquals("g1=4", transaction.getGroupPattern());
-		transaction = iterator.next();
-		assertEquals(null, transaction.getGroupPattern());
-	}
+    @Test
+    public void can_read_groups() {
+        Iterator<FinancialTransaction> iterator = source.iterator();
+        FinancialTransaction transaction = iterator.next();
+        assertEquals("g1=4", transaction.getGroupPattern());
+        transaction = iterator.next();
+        assertEquals(null, transaction.getGroupPattern());
+    }
 
-	@Test
-	public void can_read_dates() {
-		Iterator<FinancialTransaction> iterator = source.iterator();
-		FinancialTransaction transaction = iterator.next();
-		assertEquals(new LocalDate(2012, 4, 13), transaction.getDate());
-		transaction = iterator.next();
-		assertEquals(new LocalDate(2010, 5, 29), transaction.getDate());
-	}
+    @Test
+    public void can_read_dates() {
+        Iterator<FinancialTransaction> iterator = source.iterator();
+        FinancialTransaction transaction = iterator.next();
+        assertEquals(new LocalDate(2012, 4, 13), transaction.getDate());
+        transaction = iterator.next();
+        assertEquals(new LocalDate(2010, 5, 29), transaction.getDate());
+    }
 }
