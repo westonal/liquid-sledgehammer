015394095c6ed6005deb8015fbb07e6e14c53727
Alan Evans <thealanevans@googlemail.com>
2015-03-20 18:50:29 +0000

    Whitespace

--- LiquidSledgehammerCore/src/com/coltsoftware/liquidsledgehammer/IterableCounter.java
+++ LiquidSledgehammerCore/src/com/coltsoftware/liquidsledgehammer/IterableCounter.java
@@ -4,19 +4,19 @@ import java.util.Iterator;
 
 public final class IterableCounter {
 
-    private IterableCounter() {
-    }
+	private IterableCounter() {
+	}
 
-    public static int count(Iterable<?> iterable) {
-        return count(iterable.iterator());
-    }
+	public static int count(Iterable<?> iterable) {
+		return count(iterable.iterator());
+	}
 
-    public static int count(Iterator<?> iterator) {
-        int count = 0;
-        while (iterator.hasNext()) {
-            iterator.next();
-            count++;
-        }
-        return count;
-    }
+	public static int count(Iterator<?> iterator) {
+		int count = 0;
+		while (iterator.hasNext()) {
+			iterator.next();
+			count++;
+		}
+		return count;
+	}
 }
