0d8cd58faaf0409835bd94416bf5918450ca066e
Alan Evans <thealanevans@googlemail.com>
2015-05-02 11:28:24 +0100

    Json financial source information from institution directory

--- LiquidSledgehammerCommandLine/src/com/coltsoftware/liquidsledgehammer/PathSourceWalker.java
+++ LiquidSledgehammerCommandLine/src/com/coltsoftware/liquidsledgehammer/PathSourceWalker.java
@@ -10,7 +10,9 @@ import java.nio.file.Path;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
 
+import com.coltsoftware.liquidsledgehammer.collections.FinancialTransactionList;
 import com.coltsoftware.liquidsledgehammer.json.JsonStreamTransactionSource;
+import com.coltsoftware.liquidsledgehammer.model.FinancialTransaction;
 import com.coltsoftware.liquidsledgehammer.model.FinancialTransactionSourceInformation;
 import com.coltsoftware.liquidsledgehammer.sources.FinancialTransactionSource;
 
@@ -19,20 +21,44 @@ public final class PathSourceWalker {
 	private PathSourceWalker() {
 	}
 
+	public static FinancialTransactionSource combineSources(
+			ArrayList<FinancialTransactionSource> sources) {
+		FinancialTransactionList result = new FinancialTransactionList();
+		for (FinancialTransactionSource source : sources)
+			for (FinancialTransaction transaction : source)
+				result.add(transaction);
+		return result;
+	}
+	
 	public static ArrayList<FinancialTransactionSource> loadAllSourcesBelowPath(
 			Path path) throws IOException {
 		final ArrayList<FinancialTransactionSource> sources = new ArrayList<FinancialTransactionSource>();
 		Files.walkFileTree(path, new FileVisitor<Path>() {
 
+			private ArrayList<FinancialTransactionSource> localSources = new ArrayList<FinancialTransactionSource>();
+			private FinancialTransactionSourceInformation currentSource;
+
 			@Override
 			public FileVisitResult postVisitDirectory(Path dir, IOException exc)
 					throws IOException {
+				sources.add(combineSources(localSources));
+				localSources.clear();
+				currentSource = null;
 				return FileVisitResult.CONTINUE;
 			}
 
 			@Override
 			public FileVisitResult preVisitDirectory(Path dir,
 					BasicFileAttributes attrs) throws IOException {
+
+				final String institutionName = dir.getFileName().toString();
+				currentSource = new FinancialTransactionSourceInformation() {
+
+					@Override
+					public String getName() {
+						return institutionName;
+					}
+				};
 				return FileVisitResult.CONTINUE;
 			}
 
@@ -40,7 +66,7 @@ public final class PathSourceWalker {
 			public FileVisitResult visitFile(Path file,
 					BasicFileAttributes attrs) throws IOException {
 				if (file.toString().endsWith(".json"))
-					sources.add(loadSource(file));
+					localSources.add(loadSource(currentSource, file));
 				return FileVisitResult.CONTINUE;
 			}
 
@@ -54,19 +80,14 @@ public final class PathSourceWalker {
 		return sources;
 	}
 
-	protected static FinancialTransactionSource loadSource(final Path file)
-			throws IOException {
+	protected static FinancialTransactionSource loadSource(
+			FinancialTransactionSourceInformation sourceInformation,
+			final Path file) throws IOException {
 		Output.output("Loading " + file);
 		InputStream stream = new FileInputStream(file.toFile());
 		try {
 			return JsonStreamTransactionSource.fromStream(stream,
-					new FinancialTransactionSourceInformation() {
-
-						@Override
-						public String getName() {
-							return file.getParent().getFileName().toString();
-						}
-					});
+					sourceInformation);
 		} finally {
 			stream.close();
 		}
