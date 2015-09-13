63a53ea568147b7f0df260c1e7cb38cc1f59863e
Alan Evans <thealanevans@googlemail.com>
2015-09-13 10:39:33 +0100

    Batch files to run example.

---
+++ liquidsledgehammerMaven/runExample.bat
@@ -0,0 +1,1 @@
+java -jar liquid-sledgehammer-command-line\target\liquid-sledgehammer-command-line-1.0-SNAPSHOT-jar-with-dependencies.jar -jsongroup example\groups.json -jsonin example\Transactions -c prompt
